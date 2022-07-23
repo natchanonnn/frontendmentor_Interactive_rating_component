7f5b47cba8e4f32f714b86d0c079b5be31e05e6d
Natchanon Wangpruek <lnwnut227@gmail.com>
2022-07-23 22:18:59 +0700

    Init interactive rating component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import icon from './images/icon-star.svg'
+import icon2 from './images/illustration-thank-you.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      isSubmitted: false,
+      select: -1
+    };
+  }
+  
+  render() {
+    if (this.state.isSubmitted) {
+      return (
+        <div className="App">
+          <div className='Center'>
+            <img src={icon2} alt="Thankyou Icon"/>
+          </div>
+          <div className='Result Center'>
+            <p className=''>
+              You selected {this.state.select+1} out of 5 
+            </p>   
+          </div>
+          <h1 className='Center'>Thank you!</h1>
+          <p className='Center op50'> We appreciate you taking the time to give a rating.
+            If you ever need more support, don't hesitate 
+            to get in touch!
+          </p>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="App">
+          <div className='UpperStar'>
+            <div className='Star'>
+              <img src={icon} alt='star'/>  
+            </div>
+          </div>
+          <h1>How did we do?</h1>
+          <p>
+            Please let us know how we did with your support
+            request. All feedback is appreciated to help us
+            improve our offering!
+          </p>
+          <div className='NumContainer'>
+          {[...Array(5)].map((i,idx) => (
+            <button 
+              className={this.state.select === idx ? 'Num active' : 'Num'}
+              key={idx}
+              onClick={() => this.setState({select: idx})}
+            >
+                {idx+1}
+            </button>
+          ))}
+          </div>
+          <button 
+            onClick={() => this.setState({isSubmitted: true})}
+            className='SubmitButton'
+            >Submit</button>
+        </div>
+      );
+    }
+    
+  }
 }
 
 export default App;
